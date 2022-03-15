6baa51f9c966938f196cfc5ebfdbc8e2cb425c5a
antonGribanow <anton.gribanow@icloud.com>
2022-03-15 14:09:48 +0100

    init project

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+        showMenu();
+    }
+
+
+    public static void showMenu() {
+        System.out.println("1. Rent car");
+        System.out.println("2. Show rent cars");
+        System.out.println("3. ");
+
+    }
+}
